8f59778a0446dd98fcae0be3b09bfdb37b43b763
koenhoeymans <koenhoeymans@gmail.com>
2013-08-08 16:38:59 +0200

    improved test explanation

--- EndToEndTests/SampleUseTest.php
+++ EndToEndTests/SampleUseTest.php
@@ -9,51 +9,73 @@ class Epa_EndToEndTests_SampleUseTest extends PHPUnit_Framework_TestCase
 	 */
 	public function sampleApplicationUsingEpa()
 	{
+		# First some definitions. A plugin registers one or more callbacks for an event.
+		# An event is an object that is 'thrown' when you want to notify that
+		# something of potential interest is going to happen/is happening/happened.
+		# Observers are notified when an event happens.
+
+		# We'll use a login class that is observable. It implements the `Observable`
+		# interface meaning we can add observers to it. These are notified of login
+		# attempts.
 		$login = new \Epa\EndToEndTests\Support\Login();
 
-		# # observer style
+		# # Observer style
 		#
-		# Add an observer that wants to get notified to a class that supports
-		# event notification.
+		# We can add an observer to our login directly. It implements the
+		# `Observer` interface. It then gets notified of any event happening.
 
 		$logObserver = new \Epa\EndToEndTests\Support\FailedLoginLogger();
 		$login->addObserver($logObserver);
 
-		# # plugin style
+		$login->login('foo', 'bar');
+		$this->assertEquals('failure for foo:bar', $logObserver->getLog());
+
+		# # Plugin style
 		#
-		# The EventDispatcher class is also an observer that can be added
-		# like the above `SuccessLoginLogger`. It dispatches
-		# the events to plugins that register themselves to get
-		# notified when these events do occur. It provides a central point
-		# to register plugins.
+		# The EventDispatcher is a class that is also an observer and thus can
+		# be added like the above `SuccessLoginLogger`. It listens to events
+		# but dispatches the events to plugins. Plugins register themselves with
+		# the EventDispatcher with `registerPlugin`. The EventDispatcher then calls
+		# them back giving these plugins the chance to register callbacks for certain
+		# events. That means that instead it listens to events (like the log observer
+		# above, and then notifies all callbacks only when a certain event happened
+		# they are interested in. It provides a central point
+		# to register plugins to have callbacks notified of specific events.
 		#
-		# Plugins register callbacks to events. The registration is by
+		# Plugins register callbacks to events and this registration is by
 		# the class name of the events (this can be changed, see below).
 
 		$eventDispatcher = new \Epa\EventDispatcher();
 		$login->addObserver($eventDispatcher); // EventDispatcher = observer style
 
+		# The `SuccessLoginLogger` implements the `Plugin` interface and
+		# can thus be registered with the EventDispatcher and adds a callback
+		# for a certain event:
+		#
+		#     $mapper->registerForEvent(
+		#         'Epa\\EndToEndTests\\Support\\LoginEvent', $this->handleSuccessLogin()
+		#     );
+
 		$logPlugin = new \Epa\EndToEndTests\Support\SuccessLoginLogger();
 		$eventDispatcher->registerPlugin($logPlugin);
-
-		# both styles work
-
-		$login->login('foo', 'bar');
-		$this->assertEquals('failure for foo:bar', $logObserver->getLog());
 
 		$login->login('bar', 'baz');
 		$this->assertEquals('success for bar:baz', $logPlugin->getLog());
 
-		# It is possible to add plugins to the eventdispatcher itself. One
-		# possible use of this is to change the way events are named. We
-		# could change this from using the fully qualified class name to only
-		# the classname itself (eg `Foo` instead of `\MyNamespace\Events\Foo`).
+		# It is possible to add plugins that register callbacks for events
+		# thrown by the EventDispatcher itself. One possible use of this is to
+		# change the way events are named. We could change this from using
+		# the fully qualified class name to only the classname itself
+		# (eg `Foo` instead of `\MyNamespace\Events\Foo`). That's because the
+		# EventDispatcher throws the `Epa\\NewEventEvent` when it is notified
+		# of a new event. This event has the option to add an event name.
 
 		$eventNameChanger = new \Epa\EndToEndTests\Support\EventNameChangerPlugin();
 		$eventDispatcher->registerPlugin($eventNameChanger);
 
 		# We'll add a loginLogger that registers itself for the event `LoginEvent`
-		# instead of the standard `Epa\EndToEndTests\Support\LoginEvent`.
+		# instead of the standard `Epa\EndToEndTests\Support\LoginEvent`. The
+		# EventNameChangerPlugin added this version of event names to events.
 
 		$loginEventLogger = new \Epa\EndToEndTests\Support\LoginEventLogger();
 		$eventDispatcher->registerPlugin($loginEventLogger);
